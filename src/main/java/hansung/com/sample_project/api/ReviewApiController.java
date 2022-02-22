4ba2de02ae71fe6a52671d8ad53306c97350bb40
HyunSeong Jeong <chocomilk8604@gmail.com>
2022-02-22 14:10:32 +0900

    Updated

--- src/main/java/hansung/com/sample_project/api/ReviewApiController.java
+++ src/main/java/hansung/com/sample_project/api/ReviewApiController.java
@@ -6,16 +6,14 @@ import hansung.com.sample_project.service.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpSession;
 import java.util.List;
 
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping(value = "/review")
 public class ReviewApiController {
